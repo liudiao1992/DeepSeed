e84d69d48a1e9482e318de520559c7fb8d1c5cf7
Reon <775362349@qq.com>
2024-04-23 16:14:56 +0800

    first

---
+++ deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.12.7+40342055'
+git_hash='40342055'
+git_branch='fix/windows-build'
+installed_ops={'async_io': False, 'fused_adam': False, 'cpu_adam': False, 'cpu_adagrad': False, 'cpu_lion': False, 'evoformer_attn': False, 'fused_lamb': False, 'fused_lion': False, 'inference_core_ops': False, 'cutlass_ops': False, 'quantizer': False, 'ragged_device_ops': False, 'ragged_ops': False, 'random_ltd': False, 'sparse_attn': False, 'spatial_inference': False, 'transformer': False, 'stochastic_transformer': False, 'transformer_inference': False}
+compatible_ops={'async_io': False, 'fused_adam': True, 'cpu_adam': True, 'cpu_adagrad': True, 'cpu_lion': True, 'evoformer_attn': False, 'fused_lamb': True, 'fused_lion': True, 'inference_core_ops': True, 'cutlass_ops': True, 'quantizer': True, 'ragged_device_ops': True, 'ragged_ops': True, 'random_ltd': True, 'sparse_attn': False, 'spatial_inference': True, 'transformer': True, 'stochastic_transformer': True, 'transformer_inference': True, 'deepspeed_not_implemented': False}
+torch_info={'version': '2.0', 'bf16_support': True, 'cuda_version': '11.8', 'nccl_version': '0.0', 'hip_version': '0.0'}
